cd0c384c937c0de02f9debe86c3367528bd0dcf6
Mylan Mahieddine <mylanmahieddine@icloud.com>
2026-02-03 16:35:36 +0100

    Home page Welcome

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return <main className="flex min-h-screen flex-col items-center justify-center p-4">
+    <div className="w-full max-w-md space-y-6">
+      <h1 className="text-3xl font-bold text-center">Welcome to Cryptax</h1>
+    </div>
+    </main> 
+}
+
