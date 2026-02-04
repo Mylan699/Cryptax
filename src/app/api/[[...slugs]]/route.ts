e93c570cf9edc5fb997a54795fd23fb2f2838b6e
Mylan Mahieddine <mylanmahieddine@icloud.com>
2026-02-04 13:47:49 +0100

    redis configuration + room created in back-end

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,10 +1,24 @@
+import { redis } from '@/lib/redis'
 import { Elysia, t } from 'elysia'
+import { nanoid } from 'nanoid'
 
-const rooms = new Elysia({prefix: '/room'}).post('/create', () => {
-    console.log('Création d\'un salon !')
+const ROOM_TTL_SECONDS =60 * 10 // 10 minutes
+
+const rooms = new Elysia({ prefix: "/room" })
+  .post("/create", async () => {
+    const roomId = nanoid()
+
+    await redis.hset(`meta:${roomId}`, {
+      connected: [],
+      createdAt: Date.now(),
+    })
+
+    await redis.expire(`meta:${roomId}`, ROOM_TTL_SECONDS)
+
+    return { roomId }
 }) 
 
-const app = new Elysia({ prefix: '/api' }).use(rooms)
+const app = new Elysia({ prefix: '/api' }).use(rooms) 
 
 // .get
 // ('/user', { user: { name:'John Doe'} }) 
