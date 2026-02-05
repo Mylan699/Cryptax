795b98b7142afc83d598cd9b90fb783103350fd0
Mylan Mahieddine <mylanmahieddine@icloud.com>
2026-02-05 18:42:41 +0100

    Reading proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,5 +1,26 @@
-export const proxy = () => {
+import { NextRequest, NextResponse } from "next/server"
+import { redis } from "./lib/redis"
 
+export const proxy = async (req:NextRequest) => {
+    const pathname = req.nextUrl.pathname
+
+    const roomMatch = pathname.match(/^\/room\/([^/]+)$/)
+    if(!roomMatch) return NextResponse.redirect
+    (new URL("/", req.url))
+
+    const roomId = roomMatch[1] 
+
+    const meta = await redis.hgetall<{connected: string, createdAt: number}>(`meta:${roomId}`)
+
+    if(!meta) { return NextResponse.redirect
+    (new URL("/", req.url))
+    }
+
+    //localhost:3000/room/myprivateroom => roomName = myprivateroom
+    
+    //OVERVIEW: CHECK IF USER IS ALLOWED TO JOIN ROOM
+    // IF THEY ARE: LET THEM PASS 
+    //IF THEY NOT ARE: SEND THEM BACK TO LOBBY
 }
 
 export const config = {
